553ae3181e0be1fe55867d11b87a2ea2fe11b6f5
Collin Wittenstein <cwittens@students.uni-mainz.de>
2025-10-27 13:40:13 -0400

    working for constant dx and constant d in 2D

--- adi_2d.jl
+++ adi_2d.jl
@@ -1,6 +1,90 @@
-using Pkg
-Pkg.activate(@__DIR__)
-Pkg.instantiate()
+# using Pkg
+# Pkg.activate(@__DIR__)
+# Pkg.instantiate()
 
-Pkg.add("LinearAlgebra")
-Pkg.add("KernelAbstractions")
+# Pkg.add("LinearAlgebra")
+# Pkg.add("KernelAbstractions")
+# Pkg.add("Trixi")
+# Pkg.add("OrdinaryDiffEqTsit5")
+# Pkg.add("Plots")
+Pkg.add("SparseArrays")
+
+using LinearAlgebra
+using KernelAbstractions
+using Trixi
+using OrdinaryDiffEqTsit5
+using Plots
+using SparseArrays
+
+
+coordinates_min = (-1.0, -1.0) # minimum coordinates (min(x), min(y))
+coordinates_max = (1.0, 1.0) # maximum coordinates (max(x), max(y))
+
+gridx = range(coordinates_min[1], coordinates_max[1], length=101)
+gridy = range(coordinates_min[2], coordinates_max[2], length=101)
+
+dx = step(gridx)
+dy = step(gridy)
+
+nx = length(gridx)
+ny = length(gridy)
+
+dt = 0.5
+diffusivity() = 5.0e-2
+
+αx = diffusivity() * dt / dx^2
+αy = diffusivity() * dt / dy^2
+
+# Interior stencil
+dl_x = fill(αx, nx-1)
+d_x  = fill(-2αx, nx)
+du_x = fill(αx, nx-1)
+
+# Modify for von Neumann BC
+d_x[1] = -αx    # left boundary
+d_x[end] = -αx  # right boundary
+A_1 = Tridiagonal(dl_x, d_x, du_x)
+
+dl_y = fill(αy, ny-1)
+d_y  = fill(-2αy, ny)
+du_y = fill(αy, ny-1)
+
+# Modify for von Neumann BC
+d_y[1] = -αy    # left boundary
+d_y[end] =  -αy  # right boundary
+A_2 = Tridiagonal(dl_y, d_y, du_y)
+
+
+
+
+U = [initial_condition_diffusion(x, y) for x in gridx, y in gridy] # defined in elixir_advection_diffusion.jl
+heatmap(gridx, gridy, U', title="Initial Condition", xlabel="x", ylabel="y", colorbar_title="u", aspect_ratio=1)
+
+RHS = zero(U)
+u_new = zero(U)
+T = 2.0
+n_steps = Int(T / 2dt)
+for _ in 1:n_steps
+# X direction implicit, Y direction explicit
+# For each row i, apply (I + Δt A_2) to u_old[i,:]
+for i in 1:nx
+    RHS[i,:] = (I + A_2) * U[i,:]  # B1 is ny×ny, u_old[i,:] is length ny
+end
+
+for j in 1:ny
+    u_new[:,j] = (I - A_1) \ RHS[:,j]  # M1 is nx×nx, RHS[:,j] is length nx
+end
+
+
+# Y direction implicit, X direction explicit
+for j in 1:ny
+    RHS[:,j] = (I + A_1) * u_new[:,j]  # B2 is nx×nx, u_new[:,j] is length nx
+end
+
+for i in 1:nx
+    U[i, :] = (I - A_2) \ RHS[i, :]  # M2 is ny×ny, RHS[:,i] is length ny
+end
+
+end
+
+heatmap(gridx, gridy, U', title="Solution at final time", xlabel="x", ylabel="y", colorbar_title="u", aspect_ratio=1, clim=(1,1.3))
---
+++ elixir_advection_diffusion.jl
@@ -0,0 +1,88 @@
+using OrdinaryDiffEqTsit5
+using Trixi
+using Plots
+
+###############################################################################
+# semidiscretization of the linear advection-diffusion equation
+
+advection_velocity = (0.0, 0.0)
+equations = LinearScalarAdvectionEquation2D(advection_velocity)
+diffusivity() = 5.0e-2
+equations_parabolic = LaplaceDiffusion2D(diffusivity(), equations)
+
+# Create DG solver with polynomial degree = 3 and (local) Lax-Friedrichs/Rusanov flux as surface flux
+solver = DGSEM(polydeg = 3, surface_flux = flux_lax_friedrichs)
+
+coordinates_min = (-1.0, -1.0) # minimum coordinates (min(x), min(y))
+coordinates_max = (1.0, 1.0) # maximum coordinates (max(x), max(y))
+
+# Create a uniformly refined mesh with periodic boundaries
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level = 4,
+                periodicity = false,
+                n_cells_max = 30_000) # set maximum capacity of tree data structure
+
+# Define initial condition
+function initial_condition_diffusion(x, y)
+    # Store translated coordinate for easy use of exact solution
+    x_shift = [x, y] .- [0.2, 0.2]
+    nu = diffusivity()
+    c = 1
+    scalar = c + exp(-4 * sum(abs2, x_shift))
+    return scalar
+end
+
+function initial_condition_diffusion(x, t,
+                                                      equation::LinearScalarAdvectionEquation2D)
+    scalar = initial_condition_diffusion(x...)
+    return SVector(scalar)
+end
+
+
+initial_condition = initial_condition_diffusion
+
+# define periodic boundary conditions everywhere
+boundary_conditions = boundary_condition_periodic
+boundary_conditions_parabolic = boundary_condition_periodic
+
+boundary_conditions = BoundaryConditionDirichlet((x, t, equations_parabolic) -> SVector(1.0))
+boundary_conditions_parabolic = BoundaryConditionNeumann((x, t, equations_parabolic) -> SVector(0.0))
+
+# A semidiscretization collects data structures and functions for the spatial discretization
+semi = SemidiscretizationHyperbolicParabolic(mesh,
+                                             (equations, equations_parabolic),
+                                             initial_condition, solver;
+                                            #  solver_parabolic = ViscousFormulationBassiRebay1(),
+                                             boundary_conditions = (boundary_conditions,
+                                                                    boundary_conditions_parabolic))
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+# Create ODE problem with time span from 0.0 to 1.5
+tspan = (0.0, 2.0)
+ode = semidiscretize(semi, tspan)
+
+# At the beginning of the main loop, the SummaryCallback prints a summary of the simulation setup
+# and resets the timers
+summary_callback = SummaryCallback()
+
+# The AnalysisCallback allows to analyse the solution in regular intervals and prints the results
+analysis_interval = 100
+analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
+
+# The AliveCallback prints short status information in regular intervals
+alive_callback = AliveCallback(analysis_interval = analysis_interval)
+
+# Create a CallbackSet to collect all callbacks such that they can be passed to the ODE solver
+callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback)
+
+###############################################################################
+# run the simulation
+
+# OrdinaryDiffEq's `solve` method evolves the solution in time and executes the passed callbacks
+alg = Tsit5()
+time_int_tol = 1.0e-11
+sol = solve(ode, alg; abstol = time_int_tol, reltol = time_int_tol,
+            ode_default_options()..., callback = callbacks)
+plot(sol, clim=(1,1.3), title = "Solution Trixi")
